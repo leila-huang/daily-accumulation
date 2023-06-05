b3d736f23c223870342b66a38486c9ba39aa8800
喵帕斯 <leila329@163.com>
2023-06-05 17:44:45 +0800

    feat: 1.深过滤的实现
    Signed-off-by: 黄流洁 <leila329@163.com>

--- src/pages/DeepOperation/index.tsx
+++ src/pages/DeepOperation/index.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Button, Card, Space, Typography } from "antd";
 
 const DeepOperation: React.FC = () => {
-  return <></>
-}
+  const [newObj, setNewObj] = useState({});
+  const obj = {
+    name: "John",
+    age: 30,
+    address: {
+      street: "123 Main St",
+      city: "Anytown",
+      state: "CA",
+    },
+  };
+  const deepFilter = (obj: object, filterKey: string[]) => {
+    if (typeof obj !== "object" || obj === null) {
+      return obj;
+    }
 
+    const result = Array.isArray(obj) ? [] : {};
 
-export default DeepOperation
+    for (const key in obj) {
+      if (filterKey.includes(key)) {
+        continue;
+      }
+
+      const value = obj?.[key];
+      result[key] = deepFilter(value, filterKey);
+    }
+
+    return result;
+  };
+
+  return (
+    <Card title="数据">
+      <Space direction="horizontal">
+        <Typography.Paragraph>
+          {JSON.stringify(obj, null, 2)}
+        </Typography.Paragraph>
+        <Typography.Paragraph>
+          {JSON.stringify(newObj, null, 2)}
+        </Typography.Paragraph>
+      </Space>
+      <Button onClick={() => setNewObj(deepFilter(obj, ["age", "state"]))}>
+        深过滤
+      </Button>
+      <Button>深对比</Button>
+      <Button>深拷贝</Button>
+    </Card>
+  );
+};
+
+export default DeepOperation;
